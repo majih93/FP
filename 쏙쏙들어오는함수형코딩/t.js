4711b845c7b782760207c9353f75aa8cbeee8ced
majih93 <t90laser93@gmail.com>
2024-06-23 23:25:37 +0900

    쏙쏙 들어오는 함수형 코딩 5장 마무리

---
+++ 쏙쏙들어오는함수형코딩/t.js
@@ -0,0 +1,15 @@
+function update_shipping_icons(cart) {
+  var buy_buttons = get_buy_buttons_dom(cart);
+
+  for (var i = 0; i < buy_buttons.length; i++) {
+    var button = buy_buttons[i];
+    var item = button.item;
+    var new_cart = add_item(new_cart, item);
+
+    if (gets_free_shipping(new_cart)) {
+      button.show_free_shipping_icon();
+    } else {
+      button.hide_free_shipping_icon();
+    }
+  }
+}
