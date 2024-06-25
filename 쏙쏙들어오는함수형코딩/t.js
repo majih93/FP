cf6a7429b3c8895f4a81c186ea653995f370ce1f
majih93 <t90laser93@gmail.com>
2024-06-26 01:13:14 +0900

    [쏙들함코]6장 - copy_on_write

--- 쏙쏙들어오는함수형코딩/t.js
+++ 쏙쏙들어오는함수형코딩/t.js
@@ -1,15 +1,31 @@
-function update_shipping_icons(cart) {
-  var buy_buttons = get_buy_buttons_dom(cart);
+// 카피 온 라이트 형식으로 코드 변경하기
 
-  for (var i = 0; i < buy_buttons.length; i++) {
-    var button = buy_buttons[i];
-    var item = button.item;
-    var new_cart = add_item(new_cart, item);
+var mailing_list = [];
 
-    if (gets_free_shipping(new_cart)) {
-      button.show_free_shipping_icon();
-    } else {
-      button.hide_free_shipping_icon();
-    }
-  }
+function add_contact(email) {
+  mailing_list.push(email);
+}
+
+function submit_form_handler(event) {
+  var form = event.target;
+  var email = form.elements(["email"]).value;
+  add_contact(email);
+}
+
+// 1. add_contact가 전역 변수에 접근하지 않도록 한다. 활용하려는 배열을 인자로 받아서 카피온라이트 하기
+// 2. add_contact()값의 리턴값을 전역변수에 할당.
+
+function add_contact_with_copy_on_write(mailing_list_array, email) {
+  const result = [...mailing_list_array]; // copy
+
+  result.push(email); // write on copy
+
+  return result; // return copy
+}
+
+function submit_form_handler_improved(event) {
+  var form = event.target;
+  var email = form.elements(["email"]).value;
+  // 전역변수에 업데이트된 복사본 할당
+  mailing_list = add_contact_with_copy_on_write(mailing_list, email);
 }
